f7f70b6ffcb3abcfce6efbb19687cc33aa410842
Carlos Davi <carlosdavi004@gmail.com>
2023-09-05 17:57:29 -0300

    Feat: Criado estrutura inicial do componente Home

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import googlestore from '@/assets/images/googleplay_download.svg';
+import appstore from '@/assets/images/appstore_download.svg';
+
+const Home = () => {
+  return (
+    <section>
+        
+    </section>
+  );
+};
+
+export default Home;
