0adadc654287714e65f62e78c76bf9e40962c66b
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-18 16:31:01 -0300

    feat: CartSummary done

--- src/components/Cart/CartSummary.tsx
+++ src/components/Cart/CartSummary.tsx
@@ -1,5 +1,32 @@
 const CartSummary = () => {
-  return <div className="bg-[#ededed] rounded-xl">CartSummary</div>;
+  return (
+    <div className="flex flex-col gap-3 bg-[#ededed] rounded-xl py-6 px-4 text-[1.25em]">
+      <p className="flex justify-between">
+        Produtos:{' '}
+        <span className="font-bold">
+          R$ {(14999.97).toLocaleString('pt-BR')}
+        </span>
+      </p>
+      <p className="flex justify-between">
+        Entrega:{' '}
+        <span className="font-bold">R$ {(36.53).toLocaleString('pt-BR')}</span>
+      </p>
+      <p className="flex justify-between">
+        Total:{' '}
+        <span className="font-bold">
+          R${' '}
+          {(14999.97 + 36.53).toLocaleString('pt-BR', {
+            style: 'decimal',
+            minimumFractionDigits: 2,
+          })}
+        </span>
+      </p>
+
+      <button className="self-end mt-4 py-2 px-6 rounded-lg text-white font-bold bg-[#2EE356] transition-all hover:scale-[1.025]">
+        Finalizar Compra
+      </button>
+    </div>
+  );
 };
 
 export default CartSummary;
