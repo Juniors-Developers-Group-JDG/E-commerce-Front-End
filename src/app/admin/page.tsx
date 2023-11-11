7184f35bcc2ebde3a98f396d21b25f063d7275fa
gguilhermelopes <guilhermelopes275@gmail.com>
2023-11-11 15:36:28 -0300

    feat: set createProduct function to integrate with backend

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,23 +1,83 @@
 'use client';
-
-import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 
 import Footer, { designer_thaissa } from '@/components/Layout/Footer';
 
-const Admin = () => {
-  const { push } = useRouter();
+enum Categories {
+  default = 'Selecione a categoria',
+  mouse = 'Mouse',
+  teclado = 'Teclado',
+  microfone = 'Microfone',
+  console = 'Console',
+  celular = 'Celular',
+  placaDeVideo = 'Placa de vídeo',
+  roteador = 'Roteador',
+}
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+const Admin = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [category, setCategory] = useState<string[]>(['Selecione a categoria']);
+  const [amount, setAmount] = useState('');
+  const [description, setDescription] = useState('');
+  const [created, setCreated] = useState(false);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleProductSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    push('/');
+    createProduct();
+  };
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+      price,
+      description,
+      amount,
+      category,
+      images: '',
+      discount: 0,
+    }),
+  };
+
+  const createProduct = async () => {
+    setLoading(true);
+    setError('');
+    try {
+      if (
+        !name ||
+        !price ||
+        !category ||
+        category[0] === 'Selecione a categoria' ||
+        !amount ||
+        !description
+      ) {
+        throw new Error('Preencha todos os campos corretamente.');
+      }
+      const response = await fetch(
+        'https://e-commerce-backend-am7w.onrender.com/api/products/create/',
+        fetchOptions,
+      );
+      if (response.status === 201) {
+        setCreated(true);
+      }
+    } catch (error) {
+      if (error instanceof Error) setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <>
       <section className="min-h-[70%] bg-[#000033] flex justify-center items-center mt-[1em]">
         <form
-          onSubmit={handleSubmit}
+          onSubmit={handleProductSubmit}
           className="bg-[#ffffff] w-[41.25em] h-[31.25em] flex flex-col items-center max-md:text-[.9em]"
         >
           <div className="mt-[1.8em] max-md:mt-0">
@@ -29,6 +89,8 @@ const Admin = () => {
                 <input
                   className="h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
                   type="text"
+                  value={name}
+                  onChange={({ target }) => setName(target.value)}
                 />
               </div>
               <div className="w-[9.375em] flex flex-col gap-[0.5em] max-md:gap-0">
@@ -39,6 +101,8 @@ const Admin = () => {
                   className="placeholder:text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
                   type="number"
                   placeholder="R$ 0,00"
+                  value={price}
+                  onChange={({ target }) => setPrice(target.value)}
                 />
               </div>
             </div>
@@ -48,15 +112,18 @@ const Admin = () => {
                   Categoria
                 </label>
                 <select
-                  defaultValue={'default'}
+                  value={category}
+                  onChange={({ target }) => setCategory([target.value])}
                   className="text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
                 >
-                  <option value="default" disabled>
-                    Selecione a categoria
-                  </option>
-                  <option value="mouse">Mouse</option>
-                  <option value="teclado">Teclado</option>
-                  <option value="microfone">Microfone</option>
+                  {Object.values(Categories).map(category => (
+                    <option
+                      disabled={category === 'Selecione a categoria'}
+                      key={category}
+                    >
+                      {category}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="w-[15.625em] flex flex-col gap-[0.5em] max-md:gap-0">
@@ -67,6 +134,8 @@ const Admin = () => {
                   className="placeholder:text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
                   type="number"
                   placeholder="0"
+                  value={amount}
+                  onChange={({ target }) => setAmount(target.value)}
                 />
               </div>
             </div>
@@ -74,13 +143,20 @@ const Admin = () => {
               <label className="font-calibri font-bold text-[1.25em]">
                 Descrição
               </label>
-              <textarea className="resize-none h-[5.75em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full" />
+              <textarea
+                className="resize-none h-[5.75em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
+                value={description}
+                onChange={({ target }) => setDescription(target.value)}
+              />
             </div>
-            <input
-              className="mt-[1em] h-[2.813em] w-full bg-[#3282B8] font-bold text-[1.15em] text-[#ffffff] uppercase cursor-pointer transition-all hover:bg-[#276690]"
+            <button
+              disabled={loading}
+              className="mt-[1em] h-[2.813em] w-full bg-[#3282B8] font-bold text-[1.15em] text-[#ffffff] uppercase cursor-pointer transition-all hover:bg-[#276690] disabled:bg-[#788e9e]"
               type="submit"
-              value="Adicionar Produto"
-            />
+            >
+              {loading ? 'Carregando' : 'Adicionar Produto'}
+            </button>
+            <span className="block float-right text-red-600">{error}</span>
           </div>
         </form>
       </section>
