86d43af483726c1d3d44497577a21e04cf517c4d
gguilhermelopes <guilhermelopes275@gmail.com>
2023-11-11 20:51:11 -0300

    feat: handle errors and set image picker component after product creation

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,13 +20,18 @@ const Admin = () => {
   const [category, setCategory] = useState<string[]>(['Selecione a categoria']);
   const [amount, setAmount] = useState('');
   const [description, setDescription] = useState('');
-  const [created, setCreated] = useState(false);
+  const [isCreated, setIsCreated] = useState(false);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
   const handleProductSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     createProduct();
+  };
+
+  const handleGetBackClick = () => {
+    setIsCreated(false);
+    setError('');
   };
 
   const fetchOptions = {
@@ -64,8 +69,13 @@ const Admin = () => {
         fetchOptions,
       );
       if (response.status === 201) {
-        setCreated(true);
-      }
+        setIsCreated(true);
+        setName('');
+        setAmount('');
+        setDescription('');
+        setPrice('');
+        setCategory(['Selecione a categoria']);
+      } else throw new Error('Houve um erro ao criar o produto.');
     } catch (error) {
       if (error instanceof Error) setError(error.message);
     } finally {
@@ -75,90 +85,109 @@ const Admin = () => {
 
   return (
     <>
-      <section className="min-h-[70%] bg-[#000033] flex justify-center items-center mt-[1em]">
-        <form
-          onSubmit={handleProductSubmit}
-          className="bg-[#ffffff] w-[41.25em] h-[31.25em] flex flex-col items-center max-md:text-[.9em]"
-        >
-          <div className="mt-[1.8em] max-md:mt-0">
-            <div className="flex gap-[4em] mb-[2.5em] max-md:gap-0 max-md:mb-0 max-md:flex-col">
-              <div className="w-[22em] flex flex-col gap-[0.5em] max-md:gap-0">
-                <label className="font-calibri font-bold text-[1.25em]">
-                  Nome
-                </label>
-                <input
-                  className="h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
-                  type="text"
-                  value={name}
-                  onChange={({ target }) => setName(target.value)}
-                />
-              </div>
-              <div className="w-[9.375em] flex flex-col gap-[0.5em] max-md:gap-0">
-                <label className="font-calibri font-bold text-[1.25em]">
-                  Preço
-                </label>
-                <input
-                  className="placeholder:text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
-                  type="number"
-                  placeholder="R$ 0,00"
-                  value={price}
-                  onChange={({ target }) => setPrice(target.value)}
-                />
-              </div>
-            </div>
-            <div className="flex justify-between mb-[2.5em] max-md:mb-0 max-md:flex-col">
-              <div className="w-[15.625em] flex flex-col gap-[0.5em] max-md:gap-0">
-                <label className="font-calibri font-bold text-[1.25em]">
-                  Categoria
-                </label>
-                <select
-                  value={category}
-                  onChange={({ target }) => setCategory([target.value])}
-                  className="text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
-                >
-                  {Object.values(Categories).map(category => (
-                    <option
-                      disabled={category === 'Selecione a categoria'}
-                      key={category}
-                    >
-                      {category}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="w-[15.625em] flex flex-col gap-[0.5em] max-md:gap-0">
-                <label className="font-calibri font-bold text-[1.25em]">
-                  Quantidade
-                </label>
-                <input
-                  className="placeholder:text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
-                  type="number"
-                  placeholder="0"
-                  value={amount}
-                  onChange={({ target }) => setAmount(target.value)}
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-[0.5em] max-md:gap-0">
-              <label className="font-calibri font-bold text-[1.25em]">
-                Descrição
-              </label>
-              <textarea
-                className="resize-none h-[5.75em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
-                value={description}
-                onChange={({ target }) => setDescription(target.value)}
-              />
-            </div>
+      <section className="min-h-[70%] bg-[#000033] flex justify-center items-center mt-[1em] ">
+        {isCreated ? (
+          <div className="bg-[#ffffff] px-8 py-16 w-[41.25em] h-[31.25em] flex flex-col items-center max-md:text-[.9em] relative">
             <button
-              disabled={loading}
-              className="mt-[1em] h-[2.813em] w-full bg-[#3282B8] font-bold text-[1.15em] text-[#ffffff] uppercase cursor-pointer transition-all hover:bg-[#276690] disabled:bg-[#788e9e]"
-              type="submit"
+              onClick={handleGetBackClick}
+              className="absolute top-0 right-0 py-3 px-6 bg-[#3282B8] font-bold text-[1.15em] text-[#ffffff] uppercase cursor-pointer transition-all hover:bg-[#276690] disabled:bg-[#788e9e]"
             >
-              {loading ? 'Carregando' : 'Adicionar Produto'}
+              Voltar
             </button>
-            <span className="block float-right text-red-600">{error}</span>
+            <span className="absolute top-0 left-0 py-3 px-6 font-bold text-[1.15em] text-green-600">
+              Produto criado com sucesso!
+            </span>
+            <h2 className="font-bold text-[1.35em] mt-4">
+              Adicione a(s) imagem(ns) para seu produto
+            </h2>
           </div>
-        </form>
+        ) : (
+          <form
+            onSubmit={handleProductSubmit}
+            className="bg-[#ffffff] w-[41.25em] h-[31.25em] flex flex-col items-center max-md:text-[.9em]"
+          >
+            <div className="mt-[1.8em] max-md:mt-0">
+              <div className="flex gap-[4em] mb-[2.5em] max-md:gap-0 max-md:mb-0 max-md:flex-col">
+                <div className="w-[22em] flex flex-col gap-[0.5em] max-md:gap-0">
+                  <label className="font-calibri font-bold text-[1.25em]">
+                    Nome
+                  </label>
+                  <input
+                    className="h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
+                    type="text"
+                    value={name}
+                    onChange={({ target }) => setName(target.value)}
+                  />
+                </div>
+                <div className="w-[9.375em] flex flex-col gap-[0.5em] max-md:gap-0">
+                  <label className="font-calibri font-bold text-[1.25em]">
+                    Preço
+                  </label>
+                  <input
+                    className="placeholder:text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
+                    type="number"
+                    placeholder="R$ 0,00"
+                    value={price}
+                    onChange={({ target }) => setPrice(target.value)}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between mb-[2.5em] max-md:mb-0 max-md:flex-col">
+                <div className="w-[15.625em] flex flex-col gap-[0.5em] max-md:gap-0">
+                  <label className="font-calibri font-bold text-[1.25em]">
+                    Categoria
+                  </label>
+                  <select
+                    value={category}
+                    onChange={({ target }) => setCategory([target.value])}
+                    className="text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
+                  >
+                    {Object.values(Categories).map(category => (
+                      <option
+                        disabled={category === 'Selecione a categoria'}
+                        key={category}
+                      >
+                        {category}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="w-[15.625em] flex flex-col gap-[0.5em] max-md:gap-0">
+                  <label className="font-calibri font-bold text-[1.25em]">
+                    Quantidade
+                  </label>
+                  <input
+                    className="placeholder:text-[#000000] h-[2.5em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
+                    type="number"
+                    placeholder="0"
+                    value={amount}
+                    onChange={({ target }) => setAmount(target.value)}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-[0.5em] max-md:gap-0">
+                <label className="font-calibri font-bold text-[1.25em]">
+                  Descrição
+                </label>
+                <textarea
+                  className="resize-none h-[5.75em] font-calibri border-solid border-[#000000] border-[.125em] rounded-[.25em] px-[1em] py-[.5em] w-full"
+                  value={description}
+                  onChange={({ target }) => setDescription(target.value)}
+                />
+              </div>
+              <button
+                disabled={loading}
+                className="mt-[1em] h-[2.813em] w-full bg-[#3282B8] font-bold text-[1.15em] text-[#ffffff] uppercase cursor-pointer transition-all hover:bg-[#276690] disabled:bg-[#788e9e]"
+                type="submit"
+              >
+                {loading ? 'Carregando' : 'Adicionar Produto'}
+              </button>
+              <span className="block float-right text-red-600 font-bold">
+                {error}
+              </span>
+            </div>
+          </form>
+        )}
       </section>
       <Footer name={designer_thaissa.name} link={designer_thaissa.link} />
     </>
